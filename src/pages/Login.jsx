3950c2832c4ee9073e233b5df551703dabc0273f
Johnny Xiao <yuxiao9282@gmail.com>
2026-02-20 19:57:46 -0800

    debug login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
     e.preventDefault();
     setError("");
     try {
-      const res = await fetch("/api/login", {
+      const res = await fetch("http://localhost:5001/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
