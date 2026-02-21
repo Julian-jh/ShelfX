2794a9d3474e2199f3724d141a1a1463292e8482
Johnny Xiao <yuxiao9282@gmail.com>
2026-02-20 19:57:07 -0800

    debug register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,7 +11,7 @@ export default function Register() {
     e.preventDefault();
     setError("");
     try {
-  const res = await fetch("http://localhost:5001/api/register", {
+      const res = await fetch("http://localhost:5001/api/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
